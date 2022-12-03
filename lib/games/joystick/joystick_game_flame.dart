a8d0b0e0ddf85e5d0e4a7718bd81d7bcedb6448d
sebastian.klaus <sebastian.klaus@droidsolutions.de>
2022-12-03 16:33:27 +0100

    added tiledmap for testing

--- lib/games/joystick/joystick_game_flame.dart
+++ lib/games/joystick/joystick_game_flame.dart
@@ -1,12 +1,19 @@
 import 'package:flame/components.dart';
 import 'package:flame/palette.dart';
 import 'package:flame_test/games/joystick/joystick_player.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:tiled/tiled.dart';
+import 'package:flame/components.dart';
+import 'package:flame_tiled/flame_tiled.dart';
 
 class JoyStickGame extends FlameGame with HasDraggables {
   late final JoystickPlayer player;
   late final JoystickComponent joystick;
+  late double mapWidth;
+  late double mapHeight;
+  late TiledComponent homeMap;
 
   void _loadJoystick() {
     final knobPaint = BasicPalette.blue.withAlpha(200).paint();
@@ -21,8 +28,19 @@ class JoyStickGame extends FlameGame with HasDraggables {
     add(joystick);
   }
 
+  Future<void> _loadMap() async {
+    homeMap = await TiledComponent.load('droidmap.tmx', Vector2(32, 32));
+    add(homeMap);
+    mapWidth = homeMap.tileMap.map.width * 32;
+    mapHeight = homeMap.tileMap.map.height * 32;
+    if (kDebugMode) {
+      print('Map loaded');
+    }
+  }
+
   @override
   Future<void> onLoad() async {
+    await _loadMap();
     overlays.add('GameMenu');
     _loadJoystick();
   }
